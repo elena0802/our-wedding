d03bd70857f7f47decb7adedfc2fb145ca19d141
elena0802 <101960873+elena0802@users.noreply.github.com>
2022-06-28 16:25:10 -0700

    audio delete

--- js/script.js
+++ js/script.js
@@ -472,7 +472,7 @@ $(document).ready(function() {
 		var audioElm = document.getElementById('audioID');	
 		
 		if (audioElm != null){
-			audioElm.muted = false;
+			audioElm.muted = true;
 		 
 			var mute_icon = $("#mute-audio").data("mute-icon");
 			var unmute_icon = $("#mute-audio").data("unmute-icon");		
